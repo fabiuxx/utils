b8dad6be473060d7f58d78e55453f86596e23009
Fabio González <fgonzalez@sepsa.com.py>
2019-10-21 08:31:11 -0300

    Funcionalidad para desplegar toda la informacion posible sobre una excepcion dada.

--- Utils/src/main/java/fa/gs/utils/misc/errors/Errors.java
+++ Utils/src/main/java/fa/gs/utils/misc/errors/Errors.java
@@ -5,7 +5,9 @@
  */
 package fa.gs.utils.misc.errors;
 
+import fa.gs.utils.misc.text.Strings;
 import fa.gs.utils.result.utils.Failure;
+import java.io.PrintStream;
 
 /**
  *
@@ -34,6 +36,52 @@ public class Errors {
         StackTraceElement[] cleanedUpStackTrace = new StackTraceElement[originalStackTrace.length - n];
         System.arraycopy(originalStackTrace, n, cleanedUpStackTrace, 0, cleanedUpStackTrace.length);
         throwable.setStackTrace(cleanedUpStackTrace);
+    }
+
+    public static void dump(PrintStream stream, Throwable thr) {
+        dumpThrowable(stream, thr, 0);
+    }
+
+    private static void dumpThrowable(PrintStream stream, Throwable thr, int ident) {
+        if (thr == null) {
+            return;
+        }
+
+        synchronized (stream) {
+            try {
+                String spaces = ident(ident);
+
+                String msg = Strings.format("%sERROR: %s / %s (%s)", spaces, thr.getMessage(), thr.getLocalizedMessage(), thr.getClass().getCanonicalName());
+                stream.println(msg);
+
+                for (StackTraceElement element : thr.getStackTrace()) {
+                    String ste = Strings.format("%s%s", spaces, element.toString());
+                    stream.println(ste);
+                }
+
+                Throwable[] suppreseds = thr.getSuppressed();
+                if (suppreseds != null && suppreseds.length > 0) {
+                    stream.println("---------------------- <START SUPRESSED> ----------------------");
+                    for (Throwable supressed : suppreseds) {
+                        dumpThrowable(stream, supressed, ident);
+                    }
+                    stream.println("----------------------- <END SUPRESSED> -----------------------");
+                }
+            } finally {
+                stream.flush();
+            }
+
+            dumpThrowable(stream, thr.getCause(), ident + 4);
+        }
+    }
+
+    private static String ident(int ident) {
+        StringBuilder builder = new StringBuilder();
+        while (ident > 0) {
+            builder.append(" ");
+            ident--;
+        }
+        return builder.toString();
     }
 
 }
