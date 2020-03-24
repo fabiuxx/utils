cdb370cf67b0679716ddc48358b6985a2e8d2c81
Fabio González <fgonzalez@sepsa.com.py>
2020-03-24 10:02:49 -0400

    Adaptadores para serializacion/deserializacion de fechas.

---
+++ Utils/src/main/java/fa/gs/utils/misc/json/serialization/adapters/Epoch2Json.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fa.gs.utils.misc.json.serialization.adapters;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import fa.gs.utils.misc.fechas.Fechas;
+import fa.gs.utils.misc.json.adapter.JsonAdapterToJson;
+import java.util.Date;
+
+/**
+ *
+ * @author Fabio A. González Sosa
+ */
+public class Epoch2Json extends JsonAdapterToJson<Date> {
+
+    @Override
+    public Class<Date> getInputConversionType() {
+        return Date.class;
+    }
+
+    @Override
+    public JsonElement adapt(Date obj, Object... args) {
+        Long epoch;
+        if (obj == null) {
+            epoch = 1L;
+        } else {
+            epoch = Fechas.toEpoch(obj);
+        }
+        return new JsonPrimitive(epoch);
+    }
+
+}
---
+++ Utils/src/main/java/fa/gs/utils/misc/json/serialization/adapters/Json2Epoch.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fa.gs.utils.misc.json.serialization.adapters;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import fa.gs.utils.misc.errors.Errors;
+import fa.gs.utils.misc.fechas.Fechas;
+import fa.gs.utils.misc.json.adapter.JsonAdapterFromJson;
+import java.util.Date;
+
+/**
+ *
+ * @author Fabio A. González Sosa
+ */
+public class Json2Epoch extends JsonAdapterFromJson<Date> {
+
+    @Override
+    public Class<Date> getOutputConversionType() {
+        return Date.class;
+    }
+
+    @Override
+    public Date adapt(JsonElement obj, Object... args) {
+        if (obj == null || obj.isJsonNull()) {
+            return null;
+        }
+
+        if (!obj.isJsonPrimitive()) {
+            throw Errors.illegalArgument();
+        }
+
+        JsonPrimitive primitive = obj.getAsJsonPrimitive();
+        if (!primitive.isNumber()) {
+            throw Errors.illegalArgument();
+        }
+
+        Number value = primitive.getAsNumber();
+        long epoch = value.longValue();
+        return Fechas.fromEpoch(epoch);
+    }
+
+}
