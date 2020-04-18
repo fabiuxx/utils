cadd15639735f8db66306374987fd2b5006dc19d
Fabio González <fgonzalez@sepsa.com.py>
2020-04-18 19:45:04 -0400

    Simplificacion en la creacion y configuracion de cifradores TEA1.

--- Utils/src/main/java/fa/gs/utils/crypto/Cipher_TEA1.java
+++ Utils/src/main/java/fa/gs/utils/crypto/Cipher_TEA1.java
@@ -23,11 +23,39 @@ import java.math.BigInteger;
  */
 public class Cipher_TEA1 {
 
+    /**
+     * Clave de ejemplo, puede utilizarse por defecto en caso de no necesitar
+     * una clave generada desde cero.
+     */
+    public static final int[] TEA_EXAMPLE_CIPHER_KEY = {
+        0x37ffbab9, 0x3c2cec4f, 0x9f203042, 0x73c0157b,
+        0x8519678e, 0x073b8a36, 0xa95f1e96, 0x1c72e7b3,
+        0xf76991da, 0x6e452c30, 0x1d56b1bc, 0x94f8f69e,
+        0xc3c113d6, 0x137af54b, 0x5550d56f, 0x3291e0d4,
+        0x96d7f531, 0x674fdb85, 0x6460e17e, 0x69dd1877,
+        0xffcdf8a0, 0x5e44e713, 0xbec49565, 0x6bb0ba07,
+        0xc189c66c, 0xae6beb71, 0xecec2bba, 0xdb24b32e,
+        0xf39f6622, 0x8e789f64, 0xdd5de8b7, 0xe799ed0e
+    };
+
     private final static int DELTA = 0x9e3779b9;
     private final static int DECRYPT_SUM_INIT = 0xC6EF3720;
     private final static long MASK32 = (1L << 32) - 1;
+    private final int[] key;
 
-    public long encrypt(long input, int[] key) {
+    private Cipher_TEA1(int[] key) {
+        this.key = key;
+    }
+
+    public static Cipher_TEA1 instance() {
+        return instance(TEA_EXAMPLE_CIPHER_KEY);
+    }
+
+    public static Cipher_TEA1 instance(int[] key) {
+        return new Cipher_TEA1(key);
+    }
+
+    public long encrypt(long input) {
         int v1 = (int) input;
         int v0 = (int) (input >>> 32);
         int sum = 0;
@@ -39,7 +67,7 @@ public class Cipher_TEA1 {
         return (v0 & MASK32) << 32 | (v1 & MASK32);
     }
 
-    public long decrypt(long input, int[] key) {
+    public long decrypt(long input) {
         int v1 = (int) input;
         int v0 = (int) (input >>> 32);
         int sum = DECRYPT_SUM_INIT;
@@ -61,4 +89,5 @@ public class Cipher_TEA1 {
         BigInteger num = new BigInteger(value, 16);
         return num.longValue();
     }
+
 }
