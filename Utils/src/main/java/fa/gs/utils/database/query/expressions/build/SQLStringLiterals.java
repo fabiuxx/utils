f604fd9a7c0b8a2f8438e9a2620f791154062541
Fabio González <fgonzalez@sepsa.com.py>
2020-04-27 13:52:26 -0400

    [bugfix] Los valores deben estar entre comillas simples.

--- Utils/src/main/java/fa/gs/utils/database/query/expressions/build/SQLStringLiterals.java
+++ Utils/src/main/java/fa/gs/utils/database/query/expressions/build/SQLStringLiterals.java
@@ -7,6 +7,7 @@ package fa.gs.utils.database.query.expressions.build;
 
 import fa.gs.utils.misc.fechas.Fechas;
 import fa.gs.utils.misc.text.Strings;
+import fa.gs.utils.misc.text.Text;
 import java.util.Date;
 
 /**
@@ -28,15 +29,18 @@ public class SQLStringLiterals {
     }
 
     public static String fecha(Date fecha) {
-        return Fechas.toString(fecha, "yyyy-MM-dd");
+        String txt = Fechas.toString(fecha, "yyyy-MM-dd");
+        return Text.safeQuoteSingle(txt);
     }
 
     public static String hora(Date fecha) {
-        return Fechas.toString(fecha, "HH:mm:ss");
+        String txt = Fechas.toString(fecha, "HH:mm:ss");
+        return Text.safeQuoteSingle(txt);
     }
 
     public static String fechaHora(Date fecha) {
-        return Fechas.toString(fecha, "yyyy-MM-dd HH:mm:ss");
+        String txt = Fechas.toString(fecha, "yyyy-MM-dd HH:mm:ss");
+        return Text.safeQuoteSingle(txt);
     }
 
 }
