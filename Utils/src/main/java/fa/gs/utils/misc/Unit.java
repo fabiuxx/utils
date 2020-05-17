7d09996053641a295e9fa8ecabf8bf82b1a8a7d8
Fabio González <fgonzalez@sepsa.com.py>
2020-05-17 15:48:32 -0400

    Funcionalidades basicas para ejecucion de bloques anonimos y captura de excepciones no controladas utilizando valores por defecto.

---
+++ Utils/src/main/java/fa/gs/utils/misc/Unit.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fa.gs.utils.misc;
+
+/**
+ *
+ * @author Fabio A. González Sosa
+ * @param <T> Parametro de tipo.
+ */
+@FunctionalInterface
+public interface Unit<T> {
+
+    T execute() throws Throwable;
+
+}
