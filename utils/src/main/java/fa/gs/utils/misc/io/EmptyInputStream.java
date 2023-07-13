6a1d473610f2791d19f35e10eb945b278b8ad5e1
Fabio González <fgonzalez@sepsa.com.py>
2023-07-13 17:51:14 -0400

    Nada muy significativo.

--- utils/src/main/java/fa/gs/utils/misc/io/EmptyInputStream.java
+++ utils/src/main/java/fa/gs/utils/misc/io/EmptyInputStream.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fa.gs.utils.misc;
+package fa.gs.utils.misc.io;
 
 import java.io.IOException;
 import java.io.InputStream;
